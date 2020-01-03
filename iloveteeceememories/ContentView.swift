57e987d4eef1eab0176684df59f8f2c4ce44f35e
Joseph Mckenzie <joseph.mckenzie@poetic.io>
2020-01-02 20:04:46 -0500

    Initial Commit

---
+++ iloveteeceememories/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  iloveteeceememories
+//
+//  Created by Joseph Mckenzie on 1/2/20.
+//  Copyright © 2020 Joseph Mckenzie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
